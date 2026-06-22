afff17763ac544eef8d7b3a13b5698016137492f
Arun shahi <arunshahi677@gmail.com>
2026-06-23 00:34:58 +0545

    Link navbar and home components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'; // Adjust the path if they are in a components folder
+import Home from './Home';
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
